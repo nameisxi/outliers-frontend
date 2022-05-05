89dd91dc21c0f329e6fc1fcdc85fe36de04de211
Tuomas Peltonen <tuomas.j.j.peltonen@gmail.com>
2022-05-05 14:28:11 +0900

    Changed filter values url endpoint

--- src/LeadsView/dataLoader.js
+++ src/LeadsView/dataLoader.js
@@ -11,7 +11,7 @@ function getLeads(setState, setLoading, filters) {
         });
     });
 
-    const uniqueValuesUrl = `${CONFIGS.HOST}/technologies/unique-values/`;
+    const uniqueValuesUrl = `${CONFIGS.HOST}/technologies/filter-values/`;
 
     console.log("URL:", url);
 
